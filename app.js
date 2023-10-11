2d0abb396dc2739a3f2ca3a44a53124eed52b079
MarcelaZenteno93 <126695026+MarcelaZenteno93@users.noreply.github.com>
2023-10-11 19:42:43 +0000

    terminado generador de dominios

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Cualquier string");
+let pronoun =['the', 'our'];
+let adj = ['great' , 'big'];
+let noun = ['jogger', 'racoon'];
+
+for(let i = 0; i < pronoun.length; i++){
+  for(let j = 0; j < adj.length; j++){
+    for (let k =0; k < noun.length; k++){
+      console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+      console.log(pronoun[i] + adj[j] + noun[k] + '.net');
+      console.log(pronoun[i] + adj[j] + noun[k] + '.org');
+      console.log(pronoun[i] + adj[j] + noun[k] + '.io');
+
+
+    }
+  }
+}
